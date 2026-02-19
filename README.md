0b5ac2a93a44d575cf3ef2771874b33a0a7a6b0d
CaIam1Tas <3401398829@qq.com>
2026-02-20 02:28:42 +0800

    add more function declarations to class Array

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # MySTL
 a tiny c++ stl write by myself
 
+functions and classes declarations are from https://learn.microsoft.com/zh-cn/cpp/standard-library/cpp-standard-library-reference?view=msvc-170
+
 ## Goal
 have basic data container like std::array, std::vector, std::list, etc.
 
--- include/array.h
+++ include/array.h
@@ -9,15 +9,68 @@ namespace tstl {
 template <typename Ty, size_t N>
 class Array {
 public:
+	using size_type = size_t;
+	using value_type = Ty;
+	using reference = Ty&;
+	using pointer = Ty*;
+	using const_pointer = const Ty*;
+public:
 	Array();
 	Array(std::initializer_list<Ty>);
 public:
-	auto At() -> Ty&;
+	auto At(size_t index) -> Ty&;
+	auto At(size_t index) const -> const Ty&;
+	auto Back() -> Ty&;
+	auto Back() const -> const Ty&;
+	void Swap(Array<Ty, N>& other);
 private:
 	auto CheckSafe(size_t index) -> bool;
 private:
 	Ty m_array[N];
 };	// class Array
+
+
+// functions
+// Get a value by passing index and a Array
+template <size_t Index, typename Ty, size_t Size>
+auto Get(Array<Ty, Size>& array) -> Ty&;
+
+template <size_t Index, typename Ty, size_t Size>
+auto Get(const Array<Ty, Size>& array) -> const Ty&;
+
+template <size_t Index, typename Ty, size_t Size>
+auto Get(Array<Ty, Size>& array) -> Ty&&;
+
+template <size_t Index, typename Ty, size_t Size>
+auto Get(const Array<Ty, Size>& array) -> const Ty&&;
+
+template <typename Ty, size_t N>
+void Swap(Array<Ty, N>& lhs, Array<Ty, N>& rhs);
+
+template <typename Ty, size_t N>
+auto ToArray(Ty (&array)[N]) -> Array<Ty, N>;
+
+template <typename Ty, size_t N>
+auto ToArray(Ty (&&array)[N]) -> Array<Ty, N>;
+
+// operators
+template <typename Ty, size_t N>
+auto operator!=(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
+
+template <typename Ty, size_t N>
+auto operator==(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
+
+template <typename Ty, size_t N>
+auto operator<(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
+
+template <typename Ty, size_t N>
+auto operator<=(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
+
+template <typename Ty, size_t N>
+auto operator>(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
+
+template <typename Ty, size_t N>
+auto operator>=(const Array<Ty, N>& lhs, const Array<Ty, N>& rhs) -> bool;
 
 }	// namespace tstl
 
