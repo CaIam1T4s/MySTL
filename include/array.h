e28df46b77192506930fbb9d42eb8dc401443c7e
CaIam1Tas <3401398829@qq.com>
2026-02-20 00:57:51 +0800

    add some function to class 'array'

--- include/array.h
+++ include/array.h
@@ -1,4 +1,24 @@
 #ifndef ARRAY_H
 #define	ARRAY_H
 
+#include <initializer_list>
+
+// tstl = tiny standard template library
+namespace tstl {
+
+template <typename Ty, size_t N>
+class Array {
+public:
+	Array();
+	Array(std::initializer_list<Ty>);
+public:
+	auto At() -> Ty&;
+private:
+	auto CheckSafe(size_t index) -> bool;
+private:
+	Ty m_array[N];
+};	// class Array
+
+}	// namespace tstl
+
 #endif
